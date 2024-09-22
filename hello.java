f9e4da05a2cb15cfd446e78070d4cbc54c4e71cd
劉展志 <6danji@gmail.com>
2024-09-22 19:56:21 +0800

    hello反編譯

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
-public class hello{
-    public static void main(String[] args) {
-        System.out.print("hello world\n");
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class hello {
+    public hello() {
     }
-}
+ 
+    public static void main(String[] var0) {
+       System.out.print("hello world\n");
+    }
+ }
+ 
